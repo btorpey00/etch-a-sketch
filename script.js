dd5c607233a98364c224ed7e838c085eb1212713
btorpey00 <120498601+btorpey00@users.noreply.github.com>
2023-01-19 11:30:17 -0500

    Add functionality for custom colors, rainbow, and eraser

--- script.js
+++ script.js
@@ -3,6 +3,43 @@ let gridSize = document.getElementById('numRows').value;
 const gridText = document.getElementById('slideText');
 const gameboard = document.getElementById('gameboard');
 const clear = document.getElementById('clear');
+const black = document.getElementById('black');
+const rainbow = document.getElementById('rainbow');
+const shade = document.getElementById('shade');
+const erase = document.getElementById('erase');
+let custColor = document.getElementById('color');
+
+black.checked = true;
+
+rainbow.addEventListener('change', () => {
+    if (rainbow.checked) {
+        black.checked = false;
+        erase.checked = false;
+        custColor.value = '#000000';
+    }
+});
+
+black.addEventListener('change', () => {
+    if (black.checked) {
+        rainbow.checked = false;
+        erase.checked = false;
+        custColor.value = '#000000';
+    }
+});
+
+erase.addEventListener('change', () => {
+    if (rainbow.checked) {
+        black.checked = false;
+        rainbow.checked = false;
+        custColor.value = '#000000';
+    }
+});
+
+custColor.addEventListener('change', () => {
+    black.checked = false;
+    rainbow.checked = false;
+    erase.checked = false;  
+});
 
 gridText.textContent = `${gridSize} x ${gridSize}`;
 createGrid(gridSize);
@@ -43,9 +80,39 @@ function resetGrid() {
 }
 
 function colorSquare(x) {
-    x.classList.add('colored');
+    if (custColor.value != '#000000') {
+        x.style.backgroundColor = `${custColor.value}`;
+    }
+    else if (rainbow.checked) {
+        let randomNum = Math.floor(Math.random() * 6);
+        if (randomNum == 0) {
+            x.style.backgroundColor = 'red';
+        }
+        else if (randomNum == 1) {
+            x.style.backgroundColor = 'orange';
+        }
+        else if (randomNum == 2) {
+            x.style.backgroundColor = 'yellow';
+        }
+        else if (randomNum == 3) {
+            x.style.backgroundColor = 'green';
+        }
+        else if (randomNum == 4) {
+            x.style.backgroundColor = 'blue';
+        }
+        else if (randomNum == 5) {
+            x.style.backgroundColor = 'purple';
+        }
+    }
+    else if (erase.checked) {
+        x.style.backgroundColor = 'white';
+    }
+    else if (black.checked) {
+        x.style.backgroundColor = 'black';
+}
+
 }
 
 function clearAll(x) {
-    x.classList.remove('colored');
+    x.style.backgroundColor = 'white';
 }
